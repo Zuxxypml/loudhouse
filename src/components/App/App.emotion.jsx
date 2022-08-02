c08b6f0adf643e4f6e12f8cf70c75f0c792d4d10
IAMAGODDAMNHACKER <70457708+IAMAGODDAMNHACKER@users.noreply.github.com>
2022-08-02 19:10:07 +0100

    Added Basic Styles and Deps

---
+++ src/components/App/App.emotion.jsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const AppRoot = styled.div`
+  display: flex;
+  height: 100%;
+`;
+export const AppContent = styled.div`
+  flex-grow: 1;
+  padding: 2em;
+`;
+export const AppToolBar = styled.div`
+  height: 70px;
+`;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
-
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Movies from "../Movies/Movies";
+import MovieInformation from "../MovieInformation/MovieInformation";
+import Actors from "../Actors/Actors";
+import Profile from "../Profile/Profile";
+import NavBar from "../NavBar/NavBar";
+import { AppContent, AppRoot, AppToolBar } from "./App.emotion";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <AppRoot>
+      <CssBaseline />
+      <Router>
+        <NavBar />
+        <AppContent>
+          <AppToolBar>
+            <Routes>
+              <Route exact path="/" element={<h1>Home</h1>}></Route>
+              <Route exact path="/movies" element={<Movies />}></Route>
+              <Route
+                exact
+                path="/movie/:id"
+                element={<MovieInformation />}
+              ></Route>
+              <Route exact path="/actors/:id" element={<Actors />}></Route>
+              <Route exact path="/profile/:id" element={<Profile />}></Route>
+            </Routes>
+          </AppToolBar>
+        </AppContent>
+      </Router>
+    </AppRoot>
+  );
 };
 
 export default App;
